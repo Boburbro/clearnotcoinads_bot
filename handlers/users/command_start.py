19ebb96e1f902decdd2363d8067037834b6fa675
bobur <boburotaboyev0@gmail.com>
2024-05-19 19:28:24 +0500

    add github link

--- handlers/users/command_start.py
+++ handlers/users/command_start.py
@@ -6,5 +6,5 @@ from telebot.types import Message
 async def command_start(message: Message):
     await bot.send_message(
         chat_id=message.chat.id,
-        text=f"Assalomu alaykum! {message.from_user.full_name}\n\nBu bot notcoinga aloqador barcha xabarlarni o'chiradi.\n\nDasturchi: @Boburboss\n\nGithub: ",
+        text=f"Assalomu alaykum! {message.from_user.full_name}\n\nBu bot notcoinga aloqador barcha xabarlarni o'chiradi.\n\nDasturchi: @Boburboss\n\nGithub: https://github.com/Boburbro/clearnotcoinads_bot",
     )
